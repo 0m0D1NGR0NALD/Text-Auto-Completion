6da957700b634ed199c4331db27fcacee9047757
Omoding Ronald <97228745+0m0D1NGR0NALD@users.noreply.github.com>
2023-03-15 16:55:58 +0300

    Add files via upload

---
+++ data.py
@@ -0,0 +1,26 @@
+import math
+import random
+import numpy as np
+import pandas as pd
+from IPython.display import display
+import nltk
+nltk.data.path.append('.')
+
+# Opening data file to read data in file
+with open("en_US.twitter.txt", "r") as file:
+    data = file.read()
+
+# Print data type
+print("Data type:", type(data))
+# Print number of letters in dataset
+print("Number of letters:", len(data))
+# Display first 300 letters
+print("First 300 letters of the data")
+print("-------")
+display(data[0:300])
+print("-------")
+# Display last 300 letters
+print("Last 300 letters of the data")
+print("-------")
+display(data[-300:])
+print("-------")
