6a164420cdf0911d0dd368aecf78dc58af510223
Omoding Ronald <97228745+0m0D1NGR0NALD@users.noreply.github.com>
2023-03-15 17:07:11 +0300

    Update data.py

--- data.py
+++ data.py
@@ -14,19 +14,20 @@ with open("en_US.twitter.txt", "r") as file:
 print("Data type:", type(data))
 # Print number of letters in dataset
 print("Number of letters:", len(data))
-# Display first 300 letters
-print("First 300 letters of the data")
+# Display first 240 letters
+print("First 240 letters of the data")
 print("-------")
-display(data[0:300])
+display(data[0:240])
 print("-------")
-# Display last 300 letters
-print("Last 300 letters of the data")
+# Display last 240 letters
+print("Last 240 letters of the data")
 print("-------")
-display(data[-300:])
+display(data[-240:])
 print("-------")
 
 # Function to split data by linebreak
 def split_to_sentences(data):
     sentences = data.split("\n")
     sentences = [s.strip() for s in sentences]
+    sentences = [s for s in sentences if len(s) > 0]
     return sentences
