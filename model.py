7fa14655fd03d6ca9bf6cd608507a327c15ab309
Omoding Ronald <97228745+0m0D1NGR0NALD@users.noreply.github.com>
2023-03-18 12:39:29 +0300

    Update model.py

--- model.py
+++ model.py
@@ -15,3 +15,18 @@ def count_n_grams(data,n,start_token="<s>",end_token="<e>"):
                 # if not set count to one
                 n_grams[n_gram] = 1
         return n_grams
+        
+def estimate_probability(word,previous_n_gram,n_gram_counts,n_plus1_gram_counts,vocabulary_size,k=1.0):
+    if type(previous_n_gram) == list:
+        previous_n_grams = tuple(previous_n_gram)
+    else:
+        previous_n_grams = tuple([previous_n_gram])
+    previous_n_gram_count = n_gram_counts.get(previous_n_grams,0)
+    # Apply k-smoothing to handle zero counts
+    denominator = previous_n_gram_count + vocabulary_size*1
+    n_plus1_gram = previous_n_grams + tuple([word])
+    n_plus1_gram_count = n_plus1_gram_counts.get(n_plus1_gram,0)
+    # Apply smoothing
+    numerator = n_plus1_gram_count + k
+    probability = numerator/denominator
+    return probability
