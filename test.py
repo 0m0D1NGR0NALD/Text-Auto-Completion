e4c588632faf0c62a4acc5118dfd7c5602a53f93
Omoding Ronald <97228745+0m0D1NGR0NALD@users.noreply.github.com>
2023-03-20 10:07:36 +0300

    Update test.py

--- test.py
+++ test.py
@@ -2,3 +2,4 @@ from model import suggest_a_word, count_n_grams
 
 sentences = [['i', 'like', 'a', 'cat'],
              ['this', 'dog', 'is', 'like', 'a', 'cat']]
+unique_words = list(set(sentences[0] + sentences[1]))
