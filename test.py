3bce141f10b36693815dc2f758361051939607c0
Omoding Ronald <97228745+0m0D1NGR0NALD@users.noreply.github.com>
2023-03-20 10:10:08 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,16 +1,11 @@
-from model import suggest_a_word, count_n_grams
+# Function to predict multiple suggestions by looping over various n-gram models
+def get_suggestions(previous_n_gram,n_gram_counts_list,vocabulary,k=1.0,start_with=None):
+    model_counts = len(n_gram_counts_list)
+    suggestions = []
+    for i in range(model_counts-1):
+        n_gram_counts = n_gram_counts_list[i]
+        n_plus1_gram_counts = n_gram_counts_list[i+1]
 
-sentences = [['i', 'like', 'a', 'cat'],
-             ['this', 'dog', 'is', 'like', 'a', 'cat']]
-unique_words = list(set(sentences[0] + sentences[1]))
-
-unigram_counts = count_n_grams(sentences, 1)
-bigram_counts = count_n_grams(sentences, 2)
-
-previous_tokens = ["i", "like"]
-tmp_suggest1 = suggest_a_word(previous_tokens, unigram_counts, bigram_counts, unique_words, k=1.0)
-print(f"The previous words are 'i like',\n\tand the suggested word is `{tmp_suggest1[0]}` with a probability of {tmp_suggest1[1]:.4f}")
-
-tmp_starts_with = 'c'
-tmp_suggest2 = suggest_a_word(previous_tokens, unigram_counts, bigram_counts, unique_words, k=1.0, start_with=tmp_starts_with)
-print(f"The previous words are 'i like', the suggestion must start with `{tmp_starts_with}`\n\tand the suggested word is `{tmp_suggest2[0]}` with a probability of {tmp_suggest2[1]:.4f}")
+        suggestion = suggest_a_word(previous_n_gram,n_gram_counts,n_plus1_gram_counts,vocabulary,k=k,start_with=start_with)
+        suggestions.append(suggestion)
+    return suggestions
