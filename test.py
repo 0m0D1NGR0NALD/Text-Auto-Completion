6f51c1b43b20168235905df018dd54ca4340eb8e
Omoding Ronald <97228745+0m0D1NGR0NALD@users.noreply.github.com>
2023-03-20 10:07:54 +0300

    Update test.py

--- test.py
+++ test.py
@@ -3,3 +3,6 @@ from model import suggest_a_word, count_n_grams
 sentences = [['i', 'like', 'a', 'cat'],
              ['this', 'dog', 'is', 'like', 'a', 'cat']]
 unique_words = list(set(sentences[0] + sentences[1]))
+
+unigram_counts = count_n_grams(sentences, 1)
+bigram_counts = count_n_grams(sentences, 2)
