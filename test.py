14e67cb7532595017de60fb9dc1585c5c28bb9b5
Omoding Ronald <97228745+0m0D1NGR0NALD@users.noreply.github.com>
2023-03-20 10:08:54 +0300

    Update test.py

--- test.py
+++ test.py
@@ -10,3 +10,7 @@ bigram_counts = count_n_grams(sentences, 2)
 previous_tokens = ["i", "like"]
 tmp_suggest1 = suggest_a_word(previous_tokens, unigram_counts, bigram_counts, unique_words, k=1.0)
 print(f"The previous words are 'i like',\n\tand the suggested word is `{tmp_suggest1[0]}` with a probability of {tmp_suggest1[1]:.4f}")
+
+tmp_starts_with = 'c'
+tmp_suggest2 = suggest_a_word(previous_tokens, unigram_counts, bigram_counts, unique_words, k=1.0, start_with=tmp_starts_with)
+print(f"The previous words are 'i like', the suggestion must start with `{tmp_starts_with}`\n\tand the suggested word is `{tmp_suggest2[0]}` with a probability of {tmp_suggest2[1]:.4f}")
